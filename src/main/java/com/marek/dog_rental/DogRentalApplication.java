afe1fd63beb1cafb08f09c5fb50e774119a6e303
Marek Kulczyk <pz2.kulczykmarek@gmail.com>
2023-06-04 07:39:03 +0200

    initial commit

---
+++ src/main/java/com/marek/dog_rental/DogRentalApplication.java
@@ -0,0 +1,13 @@
+package com.marek.dog_rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DogRentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogRentalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marek/dog_rental/DogRentalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marek.dog_rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DogRentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
