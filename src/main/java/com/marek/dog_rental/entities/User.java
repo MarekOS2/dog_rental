59c48188e773d528b12bf7b3224802d0215a9e47
Marek Kulczyk <pz2.kulczykmarek@gmail.com>
2023-06-04 09:01:59 +0200

    added database connection and user table

--- src/main/java/com/marek/dog_rental/entities/User.java
+++ src/main/java/com/marek/dog_rental/entities/User.java
@@ -1,7 +1,24 @@
 package com.marek.dog_rental.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Getter
     private String username;
+    @Getter
     private String password;
+    @Email
+    @Getter
     private String email;
 }
