10c01ce19c4396e65e3835751edfcfa6923f2b91
Marek Kulczyk <pz2.kulczykmarek@gmail.com>
2023-06-04 09:22:00 +0200

    added validation for user entity and changed app properties

--- src/main/java/com/marek/dog_rental/entities/User.java
+++ src/main/java/com/marek/dog_rental/entities/User.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.*;
 
 @Entity
@@ -15,10 +17,13 @@ public class User {
     @GeneratedValue
     private Long id;
     @Getter
+    @NotBlank
     private String username;
     @Getter
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$",
+            message = "password not valid")
     private String password;
-    @Email
+    @Email(message = "email not valid")
     @Getter
     private String email;
 }
