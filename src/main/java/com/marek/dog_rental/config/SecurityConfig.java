f598322b12198c517df349a28d6fdf80b431c5ac
Marek Kulczyk <pz2.kulczykmarek@gmail.com>
2023-06-05 17:09:32 +0200

    added basic security config

--- src/main/java/com/marek/dog_rental/config/SecurityConfig.java
+++ src/main/java/com/marek/dog_rental/config/SecurityConfig.java
@@ -2,19 +2,21 @@ package com.marek.dog_rental.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
     @Bean
-     public UserDetailsService userDetailsService() {
-        return null;
-    }
-
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/api/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/supplier/**").hasAnyRole("SUPPLIER", "ADMIN")
+                        .requestMatchers("/api/user/**").hasAnyRole("USER", "SUPPLIER", "ADMIN")
+                        .anyRequest().authenticated()
+                );
+        return http.build();
     }
 }
